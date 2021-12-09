6ec54917af2c1b3b4f805afd20964c8f65eb58a5
Mehmet Ardıç <ardicmehmet96@gmail.com>
2021-12-09 20:44:46 +0300

    Update

--- Front/signUp/listen.js
+++ Front/signUp/listen.js
@@ -1,6 +1,8 @@
 const password = document.getElementById("password");
 const passwordRetry = document.getElementById("passwordRetry");
 const submit = document.getElementById("btn");
+const warning = document.getElementsByClassName("warning");
+
 passwordRetry.addEventListener("keypress",()=>{
    document.getElementById("btn").style.pointerEvents="auto";
    document.getElementById("btn").style.opacity="1";
@@ -8,7 +10,9 @@ passwordRetry.addEventListener("keypress",()=>{
 })
 function validateMyForm(){
    if(password.value !== passwordRetry.value){
-      alert("Şifreler Aynı değil")
+      document.getElementById("message").innerHTML = "! Şifreler Uyuşmuyor";
+      password.style.border= "2px solid red"; 
+      passwordRetry.style.border = "2px solid red";
       return false;
    }
    else{
