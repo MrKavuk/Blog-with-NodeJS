14114cf9ccfef03daddea8f47ea74bdcfc261cd1
Mehmet Ardıç <ardicmehmet96@gmail.com>
2021-12-13 14:55:50 +0300

    Big Update

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express  = require('express');
+const {controller} = require('./controller.js')
 const app = express();
+
+app.set('view engine','ejs'); // html dosyalarını ejs olarak değiştiriyoruz ve içinde javascript yazabiliyoruz
+
 
 /*Not : 1-) Eksik kısımlar modüller oluşturulduktan sonra eklenecek !!
         2-) Email kontrolü yapılmadı, onu veritabanına kaydederken kontrol et */
@@ -10,23 +14,44 @@ const app = express();
 
 const port =8080;
 
+app.listen(port , ()=>{
+    console.log("Server started working")
+})
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+
+    controller.getHomepage(req,res)
+})
+
+app.get('/home',(req,res)=>{ 
+    res.redirect('/')               //yönlendirme (sanki / isteği gelmiş gibi ona yönlendiriyor)
+})
+
 //postman body içinde mutlaka email ve şifre gönderilecek
-app.post("/api/login",(req,res)=>{
+app.get("/login",(req,res)=>{
+
+    controller.getLogin(req,res)
 
 })
 //postman body içinde mutlaka ad,soyad,email,şifre gönderilecek
-app.post("/api/signUp",(req,res)=>{
+app.get("/signUp",(req,res)=>{
+    
+    controller.getSignUp(req,res)
 
 })
 
 //query string ile email gönderilcek, gönderim şekli ?email=email123@gmail.com (postman params'a yazılıcak)
-app.get('/api/resetPassword',(req,res)=>{
+app.get('/resetPassword',(req,res)=>{
     
+    controller.getResetPassword(req,res)
+
 })
 //postman içinde params ile anlık username gönderilecek ve kayıtlı olup olmadığı sorgusu yapılacak 
-app.get('/api/usernameQuery')
+app.get('/usernameQuery')
 
 
-app.listen(port , ()=>{
-    console.log("Server started working")
+app.use((req,res)=>{
+    controller.getError(req,res)
 })
