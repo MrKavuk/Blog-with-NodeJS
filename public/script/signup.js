da769d2a9b0d54523a5386c39aed7a431b0c2500
Mehmet Ardıç <ardicmehmet96@gmail.com>
2022-01-07 17:37:52 +0300

    Update
    site decoration update

--- public/script/signup.js
+++ public/script/signup.js
@@ -1,23 +1,20 @@
 const password = document.getElementById("password");
-const passwordRetry = document.getElementById("passwordRetry");
+const passwordRetry = document.getElementById("retrypassword");
 const submit = document.getElementById("btn");
-const warning = document.getElementsByClassName("warning");
+const message =document.getElementById("message")
 
-passwordRetry.addEventListener("keypress",()=>{
-   document.getElementById("btn").style.pointerEvents="auto";
-   document.getElementById("btn").style.opacity="1";
-   
-})
 function validateMyForm(){
    if(password.value !== passwordRetry.value){
-      document.getElementById("message").innerHTML = "! Şifreler Uyuşmuyor";
-      password.style.border= "2px solid red"; 
+
+      message.innerHTML = "! Şifreler Uyuşmuyor";
+      message.style.color = "red";
+      password.style.border= "2px solid red";
       passwordRetry.style.border = "2px solid red";
-      warning[0].style.display = "inline-block"
-      warning[1].style.display = "inline-block"
+      passwordRetry.style.borderTop = "none"
       return false;
    }
    else{
+      console.log("Else icinde")
       return true;
    }
 }
