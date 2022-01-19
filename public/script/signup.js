8b139078dd80e4707ea4cecb57a0ab82fcffdf96
Mehmet Ardıç <ardicmehmet96@gmail.com>
2022-01-19 20:26:52 +0300

    Bug fix
    Fixed id error on reset password and signup pages

--- public/script/signup.js
+++ public/script/signup.js
@@ -1,21 +1,20 @@
 const password = document.getElementById("password");
-const passwordRetry = document.getElementById("retrypassword");
-const submit = document.getElementById("btn");
-const message =document.getElementById("message")
+    const passwordRetry = document.getElementById("passwordRetry");
+    const submit = document.getElementById("btn");
+    const message =document.getElementById("message")
 
-function validateMyForm(){
-   if(password.value !== passwordRetry.value){
+    function validateMyForm(){
+    if(password.value !== passwordRetry.value){
 
-      message.innerHTML = "! Şifreler Uyuşmuyor";
-      message.style.color = "red";
-      password.style.border= "2px solid red";
-      passwordRetry.style.border = "2px solid red";
-      passwordRetry.style.borderTop = "none"
-      return false;
-   }
-   else{
-      console.log("Else icinde")
-      return true;
-   }
-}
+        message.innerHTML = "! Şifreler Uyuşmuyor";
+        message.style.color = "red";
+        password.style.border= "2px solid red";
+        passwordRetry.style.border = "2px solid red";
+        return false;
+    }
+    else{
+        console.log("Else icinde")
+        return true;
+    }
+    }
   
